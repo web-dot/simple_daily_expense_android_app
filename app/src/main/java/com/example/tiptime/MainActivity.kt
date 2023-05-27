7b8536a06c10123ac9371cfdb09ca1cd30143b2c
sudipto <fedev.sudipto@gmail.com>
2023-05-27 21:58:05 +0530

    simple daily-expense app

---
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.tiptime
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var limit: Int = 0;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.setLimit.setOnClickListener { setLimit() }
+    }
+
+    private fun setLimit(){
+        val dailyLimitText = binding.dailyLimit.text.toString()
+    }
+
+
+
+}
