476c01e0edb87e7056ffd67974317672c5732dea
sudipto <cg.sudipc@gmail.com>
2023-05-28 00:21:23 +0530

    added limit and deduct amount

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.tiptime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import com.example.tiptime.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
@@ -18,9 +20,15 @@ class MainActivity : AppCompatActivity() {
         binding.setLimit.setOnClickListener { setLimit() }
     }
 
-    
+
     private fun setLimit(){
         val dailyLimitText = binding.dailyLimit.text.toString()
+        if(dailyLimitText.isNotEmpty()){
+            limit = dailyLimitText.toInt()
+            binding.limitText.text = getString(R.string.limit_text, limit)
+            binding.limitText.visibility = View.VISIBLE
+            Log.d("Main", "Limit set: $limit")
+        }
     }
 
 
